700e7c15b3b98b369e1a2045c49692df2406cfd1
Max de Vries <max.devries.1995@gmail.com>
2021-05-01 22:03:29 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,70 +1,20 @@
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
 require('dotenv').config();
 
+const port = process.env.PORT || 3000;
+const mongoUsername = encodeURIComponent(process.env.MONGODB_USERNAME);
+const mongoPassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const express = require('express');
+const mongoose = require('mongoose');
+const houses = require('./routes/houses');
+
+const app = express();
+
 app.use(express.json());
+app.use('/api/houses', houses);
 
-const homes = [
-  {
-    id: 1,
-    roomType: 'Duplex'
-  },
-  {
-    id: 2,
-    roomType: 'Flat'
-  }
-];
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
-// Get all items
-app.get('/api/listing', (req, res) => {
-  res.send(homes);
-});
-
-// Create item
-app.post('/api/listing', (req, res) => {
-  if (!req.body.roomType) return res.status(400).send('Room type is required.');
-
-  const home = {
-    id: homes.length + 1,
-    roomType: req.body.roomType,
-  };
-
-  homes.push(home);
-  res.send(home);
-});
-
-// Get item
-app.get('/api/listing/:id', (req, res) => {
-  const home = homes.find(home => home.id === parseInt(req.params.id));
-  if (!home) return res.status(404).send('The home can not be found.');
-  
-  res.send(home);
-});
-
-// Update item
-app.put('/api/listing/:id', (req, res) => {
-  const home = homes.find(home => home.id === parseInt(req.params.id));
-  if (!home) return res.status(404).send('The home can not be found.');
-
-  home.roomType = req.body.roomType;
-  res.send(home);
-});
-
-// Delete item
-app.delete('/api/listing/:id', (req, res) => {
-  const home = homes.find(home => home.id === parseInt(req.params.id));
-  if (!home) return res.status(404).send('The home can not be found.');
-
-  const index = homes.indexOf(home);
-  homes.splice(index, 1);
-  res.send(home);
-});
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+mongoose.connect(`mongodb+srv://${mongoUsername}:${mongoPassword}@reactnativecoursecluste.uxith.mongodb.net/house_listing_app?retryWrites=true&w=majority`)
+  .then(result => {
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  })
+  .catch(err => console.log(err));
